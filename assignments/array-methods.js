e8ce3ab86a72898c1193698954477f6f23dae8d9
o1123516766542617 <o1123516766542617@outlook.com>
2019-10-15 17:28:40 -0400

    add `nextProblem()` calls

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -86,6 +86,8 @@ const test = [];
 /// Use .forEach()
 ///#####################################
 
+nextProblem ("CHALLENGE");
+
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
 let fullNames = [];
 console.log(fullNames);
@@ -95,6 +97,8 @@ console.log(fullNames);
 ///-------------------------------------
 /// Use .map()
 ///#####################################
+
+nextProblem ("CHALLENGE");
 
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
 let firstNamesAllCaps = [];
@@ -106,6 +110,8 @@ console.log(firstNamesAllCaps);
 /// Use .filter()
 ///#####################################
 
+nextProblem ("CHALLENGE");
+
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
 let runnersLargeSizeShirt = [];
 console.log(runnersLargeSizeShirt);
@@ -116,6 +122,8 @@ console.log(runnersLargeSizeShirt);
 /// Use .reduce()
 ///#####################################
 
+nextProblem ("CHALLENGE");
+
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
 let ticketPriceTotal = 0;
 console.log(ticketPriceTotal);
@@ -125,6 +133,8 @@ console.log(ticketPriceTotal);
 ///-------------------------------------
 /// Be Creative
 ///#####################################
+
+nextProblem ("CHALLENGE");
 
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
