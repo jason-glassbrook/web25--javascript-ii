b82e7912018d5e22ed1ea0029292031fcf5afae3
o1123516766542617 <o1123516766542617@outlook.com>
2019-10-15 15:45:01 -0400

    add challenge headings to callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -38,32 +38,53 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
+///=== CHALLENGE 1 ===///
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
 }
 
+
+
+///=== CHALLENGE 2 ===///
+
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
 }
+
+
+
+///=== CHALLENGE 3 ===///
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
 
+
+
+///=== CHALLENGE 4 ===///
+
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
+
+
+
+///=== CHALLENGE 4 ===///
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
 }
 
-/* STRETCH PROBLEM */
+
+
+///=== STRETCH ===///
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
 }
+
+
